fb3c7bf36db21d55119a7d010f7ce08d1d54acd0
Marcus_O <marcus.ottosson.gbg22@amerikanskagymnasiet.se>
2023-01-15 23:39:17 +0100

    Added some sturcture, and library source code

---
+++ src/js/main.js
@@ -0,0 +1,61 @@
+let timeLastFrame = Date.now()
+let deltaTime = 0
+const eys = 0
+
+// Player info
+class Player{
+    constructor(){
+        this.dimentions = [40, 80]
+        this.vector = [0, 0]
+        this.position = [400, 520]
+        this.gravity = 10
+        this.inAir = false
+    }
+}
+
+let player1 = new Player()
+
+function update(){
+    clearScreen()
+
+    // Calculationg delta time
+    deltaTime = (Date.now() - timeLastFrame)/1000
+    timeLastFrame =  Date.now()
+
+    // gravity
+    player1.vector[1] += player1.gravity * deltaTime
+    
+
+    // check if on ground
+    if (player1.position[1] >= 520 && player1.vector[1] > 0){
+        player1.vector[1] = 0
+        player1.position[1] = 520
+        player1.inAir = false
+    }
+
+    // player acceleration
+    if (keyboard.d && keyboard.shift && player1.vector[0] < 4.5 && !player1.inAir)    {player1.vector[0] += 40 * deltaTime} // run  right
+    else if (keyboard.d && player1.vector[0] < 3)                                     {player1.vector[0] += 30 * deltaTime} // walk ritht
+
+    if (keyboard.a && keyboard.shift && player1.vector[0] > -4.5 && !player1.inAir)   {player1.vector[0] -= 40 * deltaTime} // run  left
+    else if (keyboard.a && player1.vector[0] > -3)                                    {player1.vector[0] -= 30 * deltaTime} // walk left
+
+    // ground friction
+    if (player1.position[1] === 520 && player1.vector[1] === 0){
+        if (player1.vector[0] <= -0.5 && !keyboard.a)	     {player1.vector[0] += 6 * deltaTime}
+        else if (player1.vector[0] >= 0.5 && !keyboard.d)    {player1.vector[0] -= 6 * deltaTime}
+        else if (Math.abs(player1.vector[0]) < 0.5 && !(keyboard.a || keyboard.d))   {player1.vector[0] = 0} // setting horisontal velocity to 0 if low enough to stop idle drifting
+    }
+
+    // player jump
+    if (keyboard.space && !player1.inAir){
+        player1.vector[1] -= 400 * deltaTime
+        player1.inAir = true
+    }
+
+    player1.position[0] += player1.vector[0]
+    player1.position[1] += player1.vector[1]
+
+    rectangle(player1.position[0], player1.position[1], player1.dimentions[0], player1.dimentions[1], "red")
+    rectangle(0, 600, 1900, 100, "black")
+}
